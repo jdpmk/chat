f340d007d7f05dc0bde723a92eefc02871e25a13
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-05-17 13:28:26 -0400

    Add client descriptor polling using select()

--- server.cc
+++ server.cc
@@ -7,90 +7,67 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <set>
 #include <vector>
 
-// TODO: Implement a thread-safe list
 std::vector<int> sockets;
-pthread_mutex_t broadcast_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t sockets_mutex;
 
-void broadcast(int source_cd, const char* msg, ssize_t len) {
-    char* formatted_msg;
-    ssize_t formatted_len;
-
-    asprintf(&formatted_msg, "[Client %d] %s", source_cd, msg);
-    formatted_len = strlen(formatted_msg);
-
-    ssize_t bytes_sent;
-
-    pthread_mutex_lock(&broadcast_mutex);
-    for (int cd : sockets) {
-        // Avoid broadcasting the message to the client which sent it
-        if (cd == source_cd) {
-            continue;
-        }
-
-        bytes_sent = send(cd,            // socket
-                          formatted_msg, // buffer to store message
-                          formatted_len, // length of message
-                          0);            // flags
-        // TODO: Gracefully detect and handle unexpected server crashes
-        //       Try checking bytes_sent == 0?
-        if (bytes_sent == -1) {
-            pthread_mutex_unlock(&broadcast_mutex);
-            perror("Unable to send message");
-            exit(1);
-        }
-    }
-    pthread_mutex_unlock(&broadcast_mutex);
-
-    free(formatted_msg);
-}
-
-void* handle_client(void* arg) {
-    int* cd_ptr = (int*) arg;
-    int cd = *cd_ptr;
-
-    char* recv_msg = new char[256];
-    ssize_t bytes_received;
+void* poller(void* arg) {
+    int error;
+    // Used to track the descriptors which have been sent to the buffer
+    std::set<int> sent;
 
     while (true) {
-        // recv(2) - receive a message on the socket
-        bytes_received = recv(cd,       // socket
-                              recv_msg, // buffer to store received message
-                              256,      // max length of buffer
-                              0);       // flags
-        if (bytes_received == 0) {
-            printf("[%d] Client has closed the connection\n", cd);
+        fd_set fds;
+        FD_ZERO(&fds);
 
-            pthread_mutex_lock(&broadcast_mutex);
-            sockets.erase(std::find(sockets.begin(), sockets.end(), cd));
-            pthread_mutex_unlock(&broadcast_mutex);
+        pthread_mutex_lock(&sockets_mutex);
+        for (int cd : sockets) {
+            FD_SET(cd, &fds);
+        }
+        pthread_mutex_unlock(&sockets_mutex);
 
-            break;
-        } else if (bytes_received < 0) {
-            perror("Unable to receive message");
-            // This thread finishes but other threads may continue
-            // Clean up memory the thread owns, and exit the thread
-            delete[] recv_msg;
-            pthread_exit(NULL);
+        // select(2) - determine which sockets are pending
+        struct timeval timeout;
+        timeout.tv_sec = 1;
+        error = select(FD_SETSIZE, // maximum number of descriptors
+                       &fds,       // read descriptors
+                       NULL,       // write descriptors
+                       NULL,       // error descriptors
+                       &timeout);  // timeout (NULL means no timeout)
+        if (error == -1) {
+            perror("[Poller] Unable to select descriptors");
+            exit(1);
         }
 
-        // recv does not null-terminate the message
-        assert(bytes_received < 256);
-        recv_msg[bytes_received] = '\0';
-
-        broadcast(cd, recv_msg, bytes_received);
+        pthread_mutex_lock(&sockets_mutex);
+        for (int cd : sockets) {
+            if (FD_ISSET(cd, &fds)) {
+                if (sent.find(cd) == sent.end()) {
+                    // If the descriptor is pending but was not sent, send it
+                    printf("[Poller] Sent client %d to be read\n", cd);
+                    sent.insert(cd);
+                }
+            } else {
+                if (sent.find(cd) != sent.end()) {
+                    // If the descriptor is not pending but was sent, remove it
+                    printf("[Poller] Client %d has been handled\n", cd);
+                    sent.erase(cd);
+                }
+            }
+        }
+        pthread_mutex_unlock(&sockets_mutex);
     }
-
-    delete[] recv_msg;
-    delete cd_ptr;
 
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
+    // Initialize values
+    pthread_mutex_init(&sockets_mutex, NULL);
+
     int error;
-    std::vector<pthread_t*> threads;
 
     // getaddrinfo(3) - set up structs
     addrinfo* res;
@@ -108,14 +85,14 @@ int main(int argc, char* argv[]) {
                         &res);
 
     if (error != 0) {
-        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(error));
+        fprintf(stderr, "[Main] getaddrinfo failed: %s\n", gai_strerror(error));
         exit(1);
     }
 
     // socket(2) - set up socket for bidirectional communication
     int sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sd == -1) {
-        perror("Unable to create socket");
+        perror("[Main] Unable to create socket");
         exit(1);
     }
 
@@ -127,62 +104,54 @@ int main(int argc, char* argv[]) {
                        (const char*) &reuse, // option value
                        sizeof(reuse));
     if (error == -1) {
-        perror("Failed to set socket options");
+        perror("[Main] Failed to set socket options");
         exit(1);
     }
 
-    std::cout << "Created socket: " << sd << std::endl;
+    printf("[Main] Created server socket %d\n", sd);
 
     // bind(2) - bind socket to a specific port (>1023)
     error = bind(sd, res->ai_addr, res->ai_addrlen);
     if (error == -1) {
-        perror("Unable to bind socket to port");
+        perror("[Main] Unable to bind socket to port");
         exit(1);
     }
 
     // listen(2) - listen for incoming connections on the socket
     error = listen(sd, 5);
     if (error == -1) {
-        perror("Unable to listen with socket");
+        perror("[Main] Unable to listen with socket");
         exit(1);
     }
 
-    std::cout << "Listening on socket: " << sd << std::endl;
+    // Poll open descriptors for pending data to read
+    pthread_t poller_thread;
+    pthread_create(&poller_thread, NULL, poller, NULL);
 
     while (true) {
         // accept(2) - accepting an incoming connection on the socket
         sockaddr_storage address;
         socklen_t address_len = sizeof(address);
+        printf("[Main] Waiting for incoming socket connection\n");
         int cd = accept(sd, (sockaddr*) &address, &address_len);
         if (cd == -1) {
-            perror("Unable to accept socket");
+            perror("[Main] Unable to accept socket");
             exit(1);
         }
 
-        std::cout << "Accepted socket: " << cd << std::endl;
+        printf("[Main] Accepted new client connection on socket %d\n", cd);
 
-        // Heap allocate client descriptor to pass as void* to pthread
-        int* _cd = new int;
-        *_cd = cd;
-
-        pthread_mutex_lock(&broadcast_mutex);
+        pthread_mutex_lock(&sockets_mutex);
         sockets.push_back(cd);
-        pthread_mutex_unlock(&broadcast_mutex);
-
-        // Process each client in a separate pthread
-        pthread_t* thread = new pthread_t;
-        threads.push_back(thread);
-        pthread_create(thread, NULL, handle_client, (void*) _cd);
+        pthread_mutex_unlock(&sockets_mutex);
     }
 
     // Clean up descriptors, memory
-    for (pthread_t* thread : threads) {
-        delete thread;
-    }
     close(sd);
     freeaddrinfo(res);
+    pthread_mutex_destroy(&sockets_mutex);
 
-    std::cout << "Clean up complete" << std::endl;
+    std::cout << "[Main] Clean up complete" << std::endl;
 
     return 0;
 }
