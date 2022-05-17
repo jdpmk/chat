7b7ff0f1e0499c13c87cb3b2810fc687b2e5a72b
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-05-17 15:19:23 -0400

    Implement producer-consumer for pending clients

--- server.cc
+++ server.cc
@@ -1,19 +1,84 @@
 #include <netdb.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <iostream>
 #include <set>
 #include <vector>
 
 std::vector<int> sockets;
 pthread_mutex_t sockets_mutex;
 
+// TODO: Use a queue as the buffer
+constexpr int N_PENDING_SOCKETS = 5;
+std::vector<int> pending_sockets;
+pthread_mutex_t pending_sockets_mutex;
+sem_t* pending_sockets_empty;
+sem_t* pending_sockets_full;
+
+// TODO: Use a queue as the buffer
+// constexpr int N_PENDING_MSGS = 5;
+std::vector<const char*> pending_msgs;
+pthread_mutex_t pending_msgs_mutex;
+sem_t* pending_msgs_empty;
+sem_t* pending_msgs_full;
+
+void* client_handler(void* arg) {
+    (void) arg;
+
+    char* recv_msg = new char[256];
+    ssize_t bytes_received;
+    int cd;
+
+    while (true) {
+        // TODO: Debug another message being sent before this resolves
+
+        // Consume the latest socket
+        sem_wait(pending_sockets_full);
+        pthread_mutex_lock(&pending_sockets_mutex);
+        cd = pending_sockets.back();
+        pending_sockets.pop_back();
+        pthread_mutex_unlock(&pending_sockets_mutex);
+        sem_post(pending_sockets_empty);
+
+        // recv(2) - receive a message on the socket
+        bytes_received = recv(cd,       // socket
+                              recv_msg, // buffer to store received message
+                              256,      // max length of buffer
+                              0);       // flags
+        if (bytes_received == 0) {
+            printf("[%d] Client has closed the connection\n", cd);
+
+            pthread_mutex_lock(&sockets_mutex);
+            sockets.erase(std::find(sockets.begin(), sockets.end(), cd));
+            pthread_mutex_unlock(&sockets_mutex);
+
+            break;
+        } else if (bytes_received < 0) {
+            perror("Unable to receive message");
+            // This thread finishes but other threads may continue
+            // Clean up memory the thread owns, and exit the thread
+            delete[] recv_msg;
+            pthread_exit(NULL);
+        }
+
+        // recv does not null-terminate the message
+        assert(bytes_received < 256);
+        recv_msg[bytes_received] = '\0';
+
+        printf("[Client Handler] Handled message from %d: %s\n", cd, recv_msg);
+    }
+
+    return NULL;
+}
+
 void* poller(void* arg) {
+    (void) arg;
+
     int error;
     // Used to track the descriptors which have been sent to the buffer
     std::set<int> sent;
@@ -48,6 +113,13 @@ void* poller(void* arg) {
                     // If the descriptor is pending but was not sent, send it
                     printf("[Poller] Sent client %d to be read\n", cd);
                     sent.insert(cd);
+
+                    // Signal to client handler threads
+                    sem_wait(pending_sockets_empty);
+                    pthread_mutex_lock(&pending_sockets_mutex);
+                    pending_sockets.push_back(cd);
+                    pthread_mutex_unlock(&pending_sockets_mutex);
+                    sem_post(pending_sockets_full);
                 }
             } else {
                 if (sent.find(cd) != sent.end()) {
@@ -66,6 +138,20 @@ void* poller(void* arg) {
 int main(int argc, char* argv[]) {
     // Initialize values
     pthread_mutex_init(&sockets_mutex, NULL);
+    pthread_mutex_init(&pending_sockets_mutex, NULL);
+    pthread_mutex_init(&pending_msgs_mutex, NULL);
+
+    // TODO: Move names to constants
+    sem_unlink("pending_sockets_empty");
+    sem_unlink("pending_sockets_full");
+    pending_sockets_empty = sem_open("pending_sockets_empty",
+                                     O_CREAT,
+                                     0,
+                                     N_PENDING_SOCKETS);
+    pending_sockets_full = sem_open("pending_sockets_full",
+                                    O_CREAT,
+                                    0,
+                                    0);
 
     int error;
 
@@ -124,9 +210,14 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // Poll open descriptors for pending data to read
+    // Initialize poller thread
     pthread_t poller_thread;
     pthread_create(&poller_thread, NULL, poller, NULL);
+
+    // Initialize client handler thread
+    // TODO: Make this a configurable thread pool
+    pthread_t client_handler_thread;
+    pthread_create(&client_handler_thread, NULL, client_handler, NULL);
 
     while (true) {
         // accept(2) - accepting an incoming connection on the socket
@@ -150,8 +241,10 @@ int main(int argc, char* argv[]) {
     close(sd);
     freeaddrinfo(res);
     pthread_mutex_destroy(&sockets_mutex);
+    pthread_mutex_destroy(&pending_sockets_mutex);
+    pthread_mutex_destroy(&pending_msgs_mutex);
 
-    std::cout << "[Main] Clean up complete" << std::endl;
+    printf("[Main] Clean up complete\n");
 
     return 0;
 }
